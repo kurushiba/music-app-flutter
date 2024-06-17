42ca52e27b7c3dcdc10c4495582d55c49e1bbff6
kurushiba <156669290+kurushiba@users.noreply.github.com>
2024-06-17 09:07:21 +0900

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:music_app/lib/spotify.dart';
+import 'package:music_app/widgets/song_card.dart';
+
+void main() async {
+  await dotenv.load(fileName: ".env");
+  await setupSpotify();
+  runApp(const MaterialApp(
+    home: MusicApp(),
+  ));
+}
+
+class MusicApp extends StatefulWidget {
+  const MusicApp({
+    super.key,
+  });
+
+  @override
+  State<MusicApp> createState() => _MusicAppState();
+}
+
+class _MusicAppState extends State<MusicApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF0E0E10),
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              'Music App',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF1C1C1E),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Icon(Icons.search,
+                              color: Colors.white, size: 24),
+                          const SizedBox(width: 8),
+                          Expanded(
+                            child: TextField(
+                              style: const TextStyle(color: Colors.white),
+                              decoration: const InputDecoration(
+                                hintText: '探したい曲を入力してください',
+                                hintStyle: TextStyle(color: Colors.white70),
+                                border: InputBorder.none,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text(
+                    'Songs',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Expanded(
+                    child: GridView.count(
+                      childAspectRatio: 0.75,
+                      crossAxisCount: 2,
+                      children: [
+                        SongCard(),
+                        SongCard(),
+                        SongCard(),
+                        SongCard(),
+                        SongCard(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+      backgroundColor: const Color(0xFF0E0E10),
+    );
+  }
+}
